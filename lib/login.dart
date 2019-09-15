77b1d1fd7fabea8bde1d31c09834f06953fafbae
Adrian Moya <adrian.moya.consultant@sothebys.com>
2019-09-14 19:08:27 -0500

    Working on login form

--- lib/login.dart
+++ lib/login.dart
@@ -1,29 +1,70 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+
+  final formKey = GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Form(
+        key: formKey,
         child: Column(
           children: [
-            Text(
-              'Usuario:',
-              style: TextStyle(fontSize: 22),
-            ),
-            TextFormField(),
-            SizedBox(height: 10,),
-            Text('Contraseña:', style: TextStyle(fontSize: 22)),
             TextFormField(
-              obscureText: true,
+              decoration: InputDecoration(labelText: 'Usuario'),
+              style: TextStyle(fontSize: 22),
+              validator: validateEmail,
+              onSaved: (value) => _email = value,
             ),
-            RaisedButton(onPressed: () {}, child: Text('Acceder'),)
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+              style: TextStyle(fontSize: 22),
+              validator: validatePassword,
+              onSaved: (value) => _password = value,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            RaisedButton(
+              onPressed: validateAndSave,
+              child: Text(
+                'Acceder',
+                style: TextStyle(fontSize: 22),
+              ),
+            ),
+            FlatButton(
+              child: Text('Registrarme', style: TextStyle(fontSize: 22)),
+              onPressed: () {},
+            )
           ],
           mainAxisAlignment: MainAxisAlignment.center,
-
         ),
       ),
     );
+  }
+
+  void validateAndSave() {
+    final form = formKey.currentState;
+    if( form.validate()) {
+      form.save();
+      print("Form is valid with values: $_email and $_password");
+    }
+  }
+
+  String validateEmail(String email) {
+    return email.isEmpty ? "El email no puede estar vacío" : null;
+  }
+
+  String validatePassword(String password) {
+    return password.isEmpty ? "La contraseña no puede estar vacía" : null;
   }
 }
