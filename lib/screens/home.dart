a67dfc5c578a31b87cbac33c4cb530f6d41f6841
Adrian Moya <adrian.moya.consultant@sothebys.com>
2019-10-06 09:16:51 -0500

    Adding side drawer

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_firebase_base/services/auth_service.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    AuthService _auth = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
       body: Text('Home Page'),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Salir'),
+              onTap: () => _auth.signOut(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
